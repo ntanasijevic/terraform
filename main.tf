4ae92cf3334def67affaaeec0d8b8258ea793232
Nebojsa Tanasijevic <nebojsa.tanasijevic@a1.digital>
2020-03-20 14:45:26 +0000

    Autoscaling groups

--- main.tf
+++ main.tf
@@ -8,10 +8,18 @@ variable "server_port" {
   default       = 8080
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-03e1b45d613694374"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-03e1b45d613694374"
   instance_type = "t2.micro"
-  vpc_security_group_ids    = [aws_security_group.instance.id]
+  security_groups    = [aws_security_group.instance.id]
 
   user_data = <<-EOF
             #!/bin/bash
@@ -19,8 +27,22 @@ resource "aws_instance" "example" {
             nohup busybox httdp -f -p ${var.server_port} &
             EOF
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy   = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration  = aws_launch_configuration.example.name
+  vpc_zone_identifier   = data.aws_subnet_ids.default.ids
+
+  min_size  = 2
+  max_size  = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -35,7 +57,3 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value         = aws_instance.example.public_ip
-  description   = "The public IP address of the web server"
-}
