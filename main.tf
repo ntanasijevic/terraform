a71df336d7c595b20427e92d6a4113b2da86a720
Nebojsa Tanasijevic <nebojsa.tanasijevic@a1.digital>
2020-03-20 14:17:00 +0000

    Variables

--- main.tf
+++ main.tf
@@ -2,11 +2,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  description   = "The port the server will use for HTTP requests"
+  type          = number
+  default       = 8080
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0cc89cd93fc3ece12"
+  ami           = "ami-03e1b45d613694374"
   instance_type = "t2.micro"
+  vpc_security_group_ids    = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.html
+            nohup busybox httdp -f -p ${var.server_port} &
+            EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value         = aws_instance.example.public_ip
+  description   = "The public IP address of the web server"
 }
